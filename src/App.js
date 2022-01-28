baadb5b434a2c8cf1d7444c2f8c4c613562a5d29
ZeroPlusKikkok <vilaiphol@hotmail.com>
2022-01-28 19:13:10 +0700

    Add React-bootstrap Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Row, Col } from 'react-bootstrap';
+import { Button, Row, Col, Card } from 'react-bootstrap';
 
 function App() {
   return (
@@ -9,13 +9,18 @@ function App() {
       <p>Resize this responsive page to see the effect!</p>
     </div>
     <div class="container mt-5">
-      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="primary" disabled>Primary</Button>{' '}
       <Button variant="secondary">Secondary</Button>{' '}
       <Button variant="success">Success</Button>{' '}
       <Button variant="warning">Warning</Button>{' '}
       <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
       <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
       <Button variant="link">Link</Button>
+    </div>
+    <div class="container mt-5">
+      <Card>
+        <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
     </div>
   </div>
   );
